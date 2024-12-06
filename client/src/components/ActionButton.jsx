decbac36ce2f5a05adc8d569c3c9885c2f01cf88
Sunil Sharma <sunilpoudel1999@gmail.com>
2024-12-06 21:27:04 +0545

    hero-image

--- client/src/components/ActionButton.jsx
+++ client/src/components/ActionButton.jsx
@@ -2,7 +2,7 @@ import { MdArrowForwardIos } from "react-icons/md";
 
 const ActionButton = () => {
     return (
-        <div className="p-4 bg-[--green] inline-flex rounded-md items-center gap-5"><span className="text-white">Getting Started</span>
+        <div className="p-4 bg-[--green] border-[--background] border-2 inline-flex rounded-md items-center gap-5 w-max hover:bg-[--background] hover:text-[--green] hover:border-[--green] cursor-pointer transition-all"><span className="text-white font-bold">GETTING STARTED</span>
             <div className="p-1 bg-white rounded-md">
                 <MdArrowForwardIos color="green" />
             </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,15 +5,15 @@ const Home = () => {
     return (
         <div className="">
             <div className="flex">
-                <div className="flex flex-col">
-                    <h2 className="md:text-4xl font-bold text-white"><span className=" text-[--green]">IMAGINATION</span> IS MORE IMPORTANT THAN KNOWLEDGE</h2>
-                    <div className=" border-l-4 border-[--green]">
-                        <span className="text-white text-lg ml-z">Lorem ipsum dolor sit amet consectetur adipisicing elit consectetur adipisicing elit.</span>
+                <div className="flex flex-col flex-wrap justify-center gap-10 max-md:items-center">
+                    <h2 className="md:text-6xl font-bold text-white"><span className=" text-[--green]">IMAGINATION</span> IS MORE IMPORTANT THAN KNOWLEDGE</h2>
+                    <div className="border-l-4 border-[--green]">
+                        <span className="text-white text-lg ml-2">Lorem ipsum dolor sit amet consectetur adipisicing elit consectetur adipisicing elit.</span>
                     </div>
                     <ActionButton />
                 </div>
-                <div>
-                    <img src="/image/author1.png" alt="" />
+                <div className="max-md:hidden">
+                    <img src="/image/author1.png" alt="" className="w-auto h-full" />
                 </div>
             </div>
         </div>
