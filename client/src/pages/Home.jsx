149965c1e2af4ebbf9a2bfe63ef8bc240c06c5de
Sunil Sharma <sunilpoudel1999@gmail.com>
2024-12-07 06:50:13 +0545

    action button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,28 +1,28 @@
 import ActionButton from "../components/ActionButton";
 
 const Home = () => {
-  return (
-    <div className="h-max w-full">
-      <div className="flex">
-        <div className="flex flex-col">
-          <h2 className="md:text-4xl font-bold text-white">
-            <span className=" text-[--green]">IMAGINATION</span> IS MORE
-            IMPORTANT THAN KNOWLEDGE
-          </h2>
-          <div className=" border-l-4 border-[--green]">
-            <span className="text-white text-lg ml-z">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit
-              consectetur adipisicing elit.
-            </span>
-          </div>
-          <ActionButton />
+    return (
+        <div className="h-max w-full">
+            <div className="flex">
+                <div className="flex flex-col justify-center gap-y-10">
+                    <h2 className="md:text-4xl font-bold text-white">
+                        <span className=" text-[--green]">IMAGINATION</span> IS MORE
+                        IMPORTANT THAN KNOWLEDGE
+                    </h2>
+                    <div className=" border-l-4 border-[--green]">
+                        <span className="text-white text-lg ml-2">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit
+                            consectetur adipisicing elit.
+                        </span>
+                    </div>
+                    <ActionButton />
+                </div>
+                <div>
+                    <img src="/image/author1.png" alt="" />
+                </div>
+            </div>
         </div>
-        <div>
-          <img src="/image/author1.png" alt="" />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
