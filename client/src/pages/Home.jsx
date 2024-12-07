7a50e16818968ef969ddcd8f25ed57d1b80a5884
Sunil Sharma <sunilpoudel1999@gmail.com>
2024-12-07 06:54:32 +0545

    home edited

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import ActionButton from "../components/ActionButton";
 
 const Home = () => {
-<<<<<<< HEAD
     return (
         <div className="h-max w-full">
             <div className="flex">
@@ -22,23 +21,6 @@ const Home = () => {
                     <img src="/image/author1.png" alt="" />
                 </div>
             </div>
-=======
-  return (
-    <div className="h-max w-full">
-      <div className="flex">
-        <div className="flex flex-col">
-          <h2 className="md:text-4xl font-bold text-white">
-            <span className=" text-[--green]">IMAGINATION</span> IS MORE
-            IMPORTANT THAN KNOWLEDGE
-          </h2>
-          <div className=" border-l-4 border-[--green]">
-            <span className="text-white text-lg ml-z">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit
-              consectetur adipisicing elit.
-            </span>
-          </div>
-          <ActionButton text={"GETTING STARTED"}/>
->>>>>>> c6c8e9e064340d089abc02fc3d2215a5e2bb751d
         </div>
     );
 };
