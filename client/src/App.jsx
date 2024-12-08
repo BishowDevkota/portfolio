09ee36fd5cc758d1cd356637b886a95024e63233
Sunil Sharma <sunilpoudel1999@gmail.com>
2024-12-08 11:16:26 +0545

    services-done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Services from "./pages/Services";
 const App = () => {
   return (
     <div className="w-full bg-[--background] h-auto">
@@ -12,6 +13,7 @@ const App = () => {
             <Home />
           </div>
           <About />
+          <Services   />
         </div>
       </div>
     </div>
